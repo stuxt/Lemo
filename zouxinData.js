cabd85b633ddcfeace23ff92af62e86913ec1481
小天同学 <c.yj666@163.com>
2017-06-23 09:51:45 +0800

    Update zouxinData.js

--- zouxinData.js
+++ zouxinData.js
@@ -1,10 +1,9 @@
-var data = {[
+var data = [{
   id:1,
-  imgUrl:"",
+  imgUrl:"src/img/a.png",
   date:"2017-06-23 星期五",
-  content:"",
-  bookName:"",
-  imgFrom:"",
-  author:""
-  ]
-}
+  content:"我是测试内容，我很文艺而且很优雅富有哲理。",
+  bookName:"我的世界，我做主",
+  imgFrom:"花瓣",
+  author:"走心书语"
+}]
