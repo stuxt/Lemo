d2447566d19106091c5059d2ceca371980932bce
小天同学 <c.yj666@163.com>
2017-06-23 10:09:55 +0800

    Update zouxinData.js

--- zouxinData.js
+++ zouxinData.js
@@ -1,4 +1,13 @@
 var data = [{
+  id:2,
+  imgUrl:"src/img/a.png",
+  date:"2017-06-25 星期五",
+  content:"我是测试内容，我很文艺而且很优雅富有哲理。3",
+  bookName:"我的世界，我做主1",
+  imgFrom:"花瓣1",
+  author:"走心书语2"
+},
+{
   id:1,
   imgUrl:"src/img/a.png",
   date:"2017-06-23 星期五",
